e066157853fa6ef079bd22f51046a2a2367f003e
KJZH001 <2802007619@qq.com>
2023-12-21 18:55:07 +0800

    Update diy-part2.sh Build with iStore

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,9 +20,15 @@
 #svn co https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang feeds/packages/lang/golang
 
 # Build istore theme
-#echo >> feeds.conf.default
-#echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
-#echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
-#./scripts/feeds update nas nas_luci
-#./scripts/feeds install -a -p nas
-#./scripts/feeds install -a -p nas_luci
+echo >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+./scripts/feeds update nas nas_luci
+./scripts/feeds install -a -p nas
+./scripts/feeds install -a -p nas_luci
+
+# Build iStore (The iStore not theme or OS)
+echo >> feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+./scripts/feeds update istore
+./scripts/feeds install -d y -p istore luci-app-store
